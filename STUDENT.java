eb482d02de9a840f0127e2448be81b171c9a50e3
jackson <jackson.dobson2026@gbwl.org>
2024-12-06 14:40:53 +0000

    declared student class

---
+++ STUDENT.java
@@ -0,0 +1,16 @@
+/* EXAMPLE CLASS
+Class names like Student are always capitalized
+Object=oriented classes are like blueprints
+(in another Java class, you create instances from this blueprint)
+Attribute = instance variable
+*/
+public class Student {
+    //1. INSTANCE VARIABLES
+    //think of what this object NEEDS to know
+    String name; //"a Student has a name"
+    int gradYear; //"the year a Student graduates"
+    //2. CONSTRUCTOR
+
+    //3. METHODS (behaviors associated with that object)
+
+}
