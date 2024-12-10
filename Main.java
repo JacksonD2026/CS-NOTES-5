8f5a1b40d285ccee6b2c75c4e864a893c03cbafd
jackson <jackson.dobson2026@gbwl.org>
2024-12-10 13:54:46 +0000

    student

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
    public static void main(String []args) {
+      System.out.println("Constructing Student Objects...");
+
+      //call DEFAULT CONSTRUCTOR
+
+      Student kinder = new Student();
+
+      //call CONSTRUCTOR with PARAMETERS
+
+      Student junior = new Student("Paige" , "Nunez" , 2026);
       
 
    }
--- STUDENT.java
+++ STUDENT.java
@@ -8,13 +8,32 @@ public class Student {
     //1. INSTANCE VARIABLES (attributes/data/fields)
     //think of what this object NEEDS to know
     // must be PRIVATE ("encapsulation")
-   private String name; //"a Student has a name"
+   private String fname; //"a Student has a name"
+   private String lname;
    private int gradYear; //"the year a Student graduates"
    private String email;
-   private boolean sports;
-   private double gpa;
+ 
+    //2. CONSTRUCTOR 
 
-    //2. CONSTRUCTOR
+    // DEFAULT CONSTRUCTOR: it's job is to initialize instance variables to default values
+   public Student() {
+        
+    fname = "First";
+    lname = "Last";
+    gradYear = 2037;
+    email = fname + "." + lname + gradYear + "gbwl.org";
+
+   }
+
+   // Constrctor with PARAMETERS
+   //Job: initialize instanec variables to provided values
+   public Student(String initFName, String initLName, int initGradYear);
+
+    fname = initFName;
+    lname = initLName;
+    gradYear = initGradYear;
+    email = fname + "." + lname + gradYear + "gbwl.org";
+
 
     //3. METHODS (behaviors associated with that object)
 
